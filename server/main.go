f8390e0e9013f5ee4a548422e36e31bf05d32310
Mark Krant <Mark Krant>
2018-03-10 11:47:02 -0800

    initial commit

---
+++ server/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"flag"
+	"github.com/gorilla/websocket"
+	"net/http"
+	"log"
+)
+
+var addr = flag.String("addr", "localhost:8080", "http service address")
+
+var upgrader = websocket.Upgrader{} // use default options
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	defer c.Close()
+	for {
+		mt, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("recv: %s", message)
+
+		str := string(message)
+		str += " hi"
+
+		err = c.WriteMessage(mt, []byte(str))
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
+}
+
+func main() {
+	flag.Parse()
+	log.SetFlags(0)
+	http.HandleFunc("/ws", echo)
+	log.Fatal(http.ListenAndServe(*addr, nil))
+}
